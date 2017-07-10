0488028e5607b32d05ed6a467bda47433d4f4f44
dilim-dfocus <dilim@dfocus.net>
2017-07-10 18:48:40 +0900

    test

---
+++ SeedTest01/Class1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SeedTest01
+{
+    public class Class1
+    {
+        //sdsdsdsdsd
+
+    }
+}
